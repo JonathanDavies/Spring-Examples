e9fc32f714d37650e83df0218b55463c6576dfed
Jonathan Davies <Jonathan.Davies@Gamesys.co.uk>
2018-04-17 08:44:47 +0100

    Liquibase Example

---
+++ spring-boot-liquibase/src/main/resources/db/changelog/001-initial-schema.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+-- changeset jon:1
+create table person (
+  id        int         not null primary key,
+  firstname varchar(80),
+  lastname  varchar(80) not null,
+  state     varchar(2)
+);
